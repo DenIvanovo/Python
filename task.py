506c5a0bb02136e901e1568d05f72d9998ac3710
DenIvanovo <54321982+DenIvanovo@users.noreply.github.com>
2019-09-30 00:25:20 +0300

    Create task.py

---
+++ task.py
@@ -0,0 +1,21 @@
+"""
+Програма запрошивает у пользователя числа до тех пор,пока пользователь не введет"СТОП"
+После этого выводит числа и их  количество.
+"""
+
+container = []
+
+while True:
+     request = input("Введите число :")
+     if request == "Стоп":
+         break
+     else:
+         if not request.isdecimal():
+             print("Ошибка ввода")
+             continue
+         else:
+             container.append(request)
+             continue
+
+print(container)
+print("Количество  " + str(len(container)))
